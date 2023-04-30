4438979edd8ad69dcf61e680ac3d5f0a357d523d
gaurangi99 <imgaurangi.99@gmail.com>
2023-04-30 18:55:12 +0530

    adding action.py & some rules

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,38 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+class ActionGiveRecipe(Action):
+    def name(self) -> Text:
+        return "action_give_recipe"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Get the ingredient entity from the user's message
+        ingredient = tracker.get_slot("ingredient")
+        url = "https://api-inference.huggingface.co/models/flax-community/t5-recipe-generation"
+        my_list = list(ingredient)
+        print(my_list)
+        payload = {
+            "inputs": json.dumps(my_list)
+        }
+        headers = {
+        'Authorization': 'Bearer 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',
+        'Content-Type': 'application/json'
+        }
+        # Make a GET request to the Chef Transformer API with the ingredient
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        # If the API returns a recipe, send it to the user
+        if response.status_code == 200:
+            recipe = response.json()[0]["generated_text"]
+            dispatcher.utter_message(text=f"Here is a recipe with {ingredient}: {recipe}")
+            SlotSet("ingredient", None)
+        # If the API returns an error, send a sorry message to the user
+        else:
+            dispatcher.utter_template("utter_sorry_no_recipe", tracker)
+
+        return []
